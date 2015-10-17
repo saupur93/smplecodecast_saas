8719702f9110abbd561127d6f45382770f7494d9
Saurav Purkayastha <sauravpurkayastha@gmail.com>
2015-10-17 16:46:45 -0700

    Added contacts table and model

---
+++ db/migrate/20151017111200_create_contracts.rb
@@ -0,0 +1,11 @@
+class CreateContracts < ActiveRecord::Migration
+  def change
+    create_table :contracts do |t|
+    	t.string :name
+    	t.string :email
+    	t.text :comments
+
+    	t.timestamps
+    end
+  end
+end
