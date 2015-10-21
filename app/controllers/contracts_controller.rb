d3bb06bd3176a4129f98056a02a826db4227dc6b
Saurav Purkayastha <sauravpurkayastha@gmail.com>
2015-10-20 18:22:29 -0700

    Added validation and create action

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -4,6 +4,18 @@ class ContractsController < ApplicationController
 	end
 
 	def create
+		@contract = Contract.new(contract_params)
+		if @contract.save
+			flash[:success] = "Message sent."
+			redirect_to new_contract_path
+		else
+			flash[:danger] = "Error occured, message has not been sent."
+			redirect_to new_contract_path
+		end
 	end
+	private
+	  def contract_params
+	  	params.require(:contract).permit(:name, :email, :comments)
+	  end
   
 end
--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,3 +1,4 @@
 class Contract < ActiveRecord::Base
-  
+  validates :name, presence: true
+  validates :email, presence: true
 end
