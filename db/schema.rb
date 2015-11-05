40247632995cdc90a94b31dc94f7c762a9bdecc7
Saurav Purkayastha <sauravpurkayastha@gmail.com>
2015-11-05 15:30:33 -0800

    Added plan model and db table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021234003) do
+ActiveRecord::Schema.define(version: 20151025091104) do
 
   create_table "contracts", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "plans", force: true do |t|
+    t.string   "name"
+    t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
